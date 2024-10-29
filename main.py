678ec06b194cd7261c24e228cb0fc61853628115
erika-JPG <erika.js.ponce@gmail.com>
2024-10-29 12:57:00 -0700

    print_report and formatted output

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     character_count = count_characters(text)
+    report = print_report(character_count)
 
-    print(character_count)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"\n{word_count} words found in the document")
+    print(f"{report} --- End report ---")
 
 def get_word_count(text):
     words = text.split()
@@ -27,5 +30,21 @@ def count_characters(text):
         else:
             character_count[letter] = 1
     return character_count
+
+def print_report(character_count_object):
+
+    report = ''
+
+    def sort_on(dict):
+        return dict["char"]
+    
+    array_of_characters = [{"char": key, "count": value} for key, value in character_count_object.items()]
+    
+    array_of_characters.sort(reverse=False, key=sort_on)
+
+    for item in array_of_characters:
+        if item["char"].isalpha():
+            report += f"The '{item["char"]}' character was found {item["count"]} times \n"
+    return report
 
 main()
